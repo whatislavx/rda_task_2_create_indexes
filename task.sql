d9aacc9bc069119dbd348aa39bfab5b53b7a17e7
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2025-11-10 23:58:07 +0200

    Solution v2

--- task.sql
+++ task.sql
@@ -1,10 +1,4 @@
 USE ShopDB; 
 
 CREATE INDEX Name ON Products (Name);
-CREATE INDEX Price ON Products (Price);
-
 CREATE INDEX Email ON Customers (Email);
-CREATE INDEX LastName ON Customers (LastName);
-
-CREATE INDEX Date ON Orders (Date);
-
